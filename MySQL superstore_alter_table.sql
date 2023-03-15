e2fc795dd13d4bdfe60723fb3a35586598ec5847
Gigi-gg <125163018+Gigi-gg@users.noreply.github.com>
2023-03-14 22:00:35 -0700

    Update MySQL superstore_alter_table.sql

--- MySQL superstore_alter_table.sql
+++ MySQL superstore_alter_table.sql
@@ -152,3 +152,16 @@ ALTER TABLE superstore
 ALTER TABLE superstore
     RENAME COLUMN new_ship_date TO ship_date,
     RENAME COLUMN new_order_date TO order_date;
+    
+ /* View new altered table */
+ 
+ SELECT * FROM superstore LIMIT 5;
+    
+| row_id | order_id       | ship_mode      | cust_id  | cust_name       | Segment   | Country       | City            | State      | zip   | Region | product_id      | Category        | Cateogory_2 | product_name                                                | Sales    | Quantity | Discount | Profit   | order_month | order_day | order_year | ship_date  | order_date |
+|--------|----------------|----------------|----------|-----------------|-----------|---------------|-----------------|------------|-------|--------|-----------------|-----------------|-------------|-------------------------------------------------------------|----------|----------|----------|----------|-------------|-----------|------------|------------|------------|
+| 1      | CA-2016-152156 | Second Class   | CG-12520 | Claire Gute     | Consumer  | United States | Henderson       | Kentucky   | 42420 | South  | FUR-BO-10001798 | Furniture       | Bookcases   | Bush Somerset Collection Bookcase                           | 261.96   | 2        | 0        | 41.9136  | 11          | 8         | 2016       | 2016-11-11 | 2016-11-08 |
+| 2      | CA-2016-152156 | Second Class   | CG-12520 | Claire Gute     | Consumer  | United States | Henderson       | Kentucky   | 42420 | South  | FUR-CH-10000454 | Furniture       | Chairs      | Hon Deluxe Fabric Upholstered Stacking Chairs, Rounded Back | 731.94   | 3        | 0        | 219.582  | 11          | 8         | 2016       | 2016-11-11 | 2016-11-08 |
+| 3      | CA-2016-138688 | Second Class   | DV-13045 | Darrin Van Huff | Corporate | United States | Los Angeles     | California | 90036 | West   | OFF-LA-10000240 | Office Supplies | Labels      | Self-Adhesive Address Labels for Typewriters by Universal   | 14.62    | 2        | 0        | 6.8714   | 6           | 12        | 2016       | 2016-06-16 | 2016-06-12 |
+| 4      | US-2015-108966 | Standard Class | SO-20335 | Sean O'Donnell  | Consumer  | United States | Fort Lauderdale | Florida    | 33311 | South  | FUR-TA-10000577 | Furniture       | Tables      | Bretford CR4500 Series Slim Rectangular Table               | 957.5775 | 5        | 0.45     | -383.031 | 10          | 11        | 2015       | 2015-10-18 | 2015-10-11 |
+| 5      | US-2015-108966 | Standard Class | SO-20335 | Sean O'Donnell  | Consumer  | United States | Fort Lauderdale | Florida    | 33311 | South  | OFF-ST-10000760 | Office Supplies | Storage     | Eldon Fold 'N Roll Cart System                              | 22.368   | 2        | 0.2      | 2.5164   | 10          | 11        | 2015       | 2015-10-18 | 2015-10-11 |
+
